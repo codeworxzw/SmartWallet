16b30dd71f69255ee1e5960b115172ca00c69fa6
burzakovskiy <burzakovskiy@gmail.com>
2015-12-02 21:36:43 +0200

    created ReportActivity toolbar

--- app/src/main/java/com/rbsoftware/pfm/personalfinancemanager/ReportActivity.java
+++ app/src/main/java/com/rbsoftware/pfm/personalfinancemanager/ReportActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -19,11 +21,34 @@ public class ReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.report_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         FM = getSupportFragmentManager();
         mPager= (ViewPager) findViewById(R.id.pager);
         mPager.setAdapter(new CollectionPagerAdapter(FM));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.report_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private class CollectionPagerAdapter extends FragmentStatePagerAdapter {
