f3e0f9e0486dcbe61f48e970d14aaf0663b07501
burzakovskiy <burzakovskiy@gmail.com>
2015-12-15 23:33:25 +0200

    Charts started

--- app/src/main/java/com/rbsoftware/pfm/personalfinancemanager/Charts.java
+++ app/src/main/java/com/rbsoftware/pfm/personalfinancemanager/Charts.java
@@ -4,6 +4,9 @@ package com.rbsoftware.pfm.personalfinancemanager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -18,6 +21,12 @@ public class Charts extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +39,23 @@ public class Charts extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         getActivity().setTitle(getResources().getStringArray(R.array.drawer_menu)[1]);
+
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        getActivity().getMenuInflater().inflate(R.menu.filter, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_filter){
+            //Do whatever you want to do
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
