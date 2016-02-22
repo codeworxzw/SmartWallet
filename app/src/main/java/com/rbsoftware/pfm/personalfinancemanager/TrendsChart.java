fd08cac8bc338bd2237f15a30cc173ec83c9b19d
rybachok <bohdan.rybachok@gmail.com>
2016-02-22 03:19:57 +0200

    date bug for trends

--- app/src/main/java/com/rbsoftware/pfm/personalfinancemanager/TrendsChart.java
+++ app/src/main/java/com/rbsoftware/pfm/personalfinancemanager/TrendsChart.java
@@ -313,158 +313,354 @@ public class TrendsChart extends Fragment {
      */
     private List<String[]> getDataFromDocument(int lineId, List<FinanceDocument> docList) {
         int value;
+        int i;
         List<String[]> data = new ArrayList<>();
         switch (lineId) {
             case R.id.popupBalance:
+                i = 0;
                 for (FinanceDocument doc : docList) {
-                    data.add(new String[]{
-                            Integer.toString(doc.getTotalIncome()-doc.getTotalExpense()),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)});
-
+                    value = doc.getTotalIncome()-doc.getTotalExpense();
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                                Log.d("Tag","");
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
             case R.id.popupTotalIncome:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getTotalIncome();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)});
-
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
             case R.id.popupTotalExpense:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getTotalExpense();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
             case R.id.popupSalary:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getSalary();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
             case R.id.popupRentalIncome:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getRentalIncome();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if ( i > 0) {
+                            if  (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i-1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                                //Log.d("DATA", data + "");
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
             case R.id.popupInterest:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getInterest();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
+
             case R.id.popupGifts:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getGifts();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
+
             case R.id.popupOtherIncome:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getOtherIncome();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
+
             case R.id.popupTaxes:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getTaxes();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
+
             case R.id.popupMortgage:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getMortgage();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
             case R.id.popupCreditCard:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getCreditCard();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
             case R.id.popupUtilities:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getUtilities();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
             case R.id.popupFood:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getFood();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
             case R.id.popupCarPayment:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getCarPayment();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
+
             case R.id.popupPersonal:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getPersonal();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
             case R.id.popupActivities:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getActivities();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
             case R.id.popupOtherExpense:
+                i = 0;
                 for (FinanceDocument doc : docList) {
                     value = doc.getOtherExpenses();
-                    if (value != 0) data.add(new String[]{
-                            Integer.toString(value),
-                            doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
-                    });
+                    if (value != 0) {
+                        if (i > 0) {
+                            if (doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT).equals(data.get(i - 1)[1])) {
+                                data.get(i - 1)[0] = String.valueOf(Integer.valueOf(data.get(i - 1)[0]) + value);
+                            }
+                        }
+                        else {
+                            data.add(new String[]{
+                                    Integer.toString(value),
+                                    doc.getNormalDate(FinanceDocument.DATE_FORMAT_SHORT)
+                            });
+                        }
+                        ++i;
+                    }
                 }
                 break;
 
